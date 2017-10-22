2a3403c5fef31206b54441fb46bb47b2aff8df83
tarellano <thomas.g.arellano@gmail.com>
2017-10-21 23:56:10 -0400

    Fix app.js indentation

--- src/js/page/app.js
+++ src/js/page/app.js
@@ -1,24 +1,23 @@
-    import { bindActionCreators } from 'redux';
-    import { connect } from 'react-redux';
-    import * as actionCreators from '../actions/action-creators';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/action-creators';
 
-    import Main from './main';
+import Main from './main';
 
-    function mapStateToProps(state) {
-      return {
-        colorCode: state.colorCode,
-        opacity: state.opacity,
-        value: state.value,
-        tolerance: state.tolerance,
-        colorTolerance: state.colorTolerance
-      }
-    }
+function mapStateToProps(state) {
+  return {
+    colorCode: state.colorCode,
+    opacity: state.opacity,
+    value: state.value,
+    tolerance: state.tolerance,
+    colorTolerance: state.colorTolerance
+  }
+}
 
-    function mapDispatchToProps(dispatch){
-      return bindActionCreators(actionCreators, dispatch);
-    }
+function mapDispatchToProps(dispatch){
+  return bindActionCreators(actionCreators, dispatch);
+}
 
-    const app = connect(mapStateToProps, mapDispatchToProps)(Main);
+const app = connect(mapStateToProps, mapDispatchToProps)(Main);
 
-    export default app;
-
+export default app;
