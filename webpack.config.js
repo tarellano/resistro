146931c6a4c04ec3892e0facc538510d807da870
tarellano <thomas.g.arellano@gmail.com>
2017-11-10 22:27:01 -0500

    Update webpack and babel dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,28 @@ var path = require('path');
 module.exports = {
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./src/index.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
-      }
-    ]
-  },
   output: {
     path: __dirname + "/dist",
     filename: "bundle.js"
   },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        include: [
+          path.join( __dirname, 'src')
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'stage-0'],
+            plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+          }
+        }
+      }
+    ]
+  },  
   plugins: debug ? [] : [
     new webpack.DefinePlugin({
       "process.env": {
