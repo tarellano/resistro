a755788912aa7972592ecf53abe2e5dc8d5f0329
tarellano <thomas.g.arellano@gmail.com>
2017-11-11 18:20:05 -0500

    Add .babelrc and .eslintrc to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,13 +19,7 @@ module.exports = {
         include: [
           path.join( __dirname, 'src')
         ],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['react', 'es2015', 'stage-0'],
-            plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
-          }
-        }
+        use: ['babel-loader', 'eslint-loader']
       },
       {
         test: /\.scss$/,        
